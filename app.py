d9bd14d19f3410fc405222df8213897e26665eed
IMJONEZZ <chrisbrousseau304@gmail.com>
2019-10-02 21:23:55 -0600

    good try

--- app.py
+++ app.py
@@ -5,12 +5,16 @@ import generation
 
 app = Flask(__name__, static_url_path='/static')
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 @app.route('/', methods=['POST'])
-def render_page():
+def predict():
+    assert request.method == 'POST'
     data = request.get_json()
     text = generation.main(model_type='gpt2', model_name_or_path='gpt2', prompt=str(data))
-    return render_template('index.html', text=jsonify(text))
-
-
+    return jsonify(text)
+    
 if __name__ == '__main__':
     app.run(port=os.getenv('PORT', 5000), debug=False)
